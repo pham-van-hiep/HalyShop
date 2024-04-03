927acc505a8621085917fb449ba8690bcc37badc
pham-van-hiep <pham-van-hiep@goodcycle.net>
2024-04-04 00:36:44 +0900

    add core file

---
+++ HaLyShop/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  HaLyShop
+//
+//  Created by Pham on 2024/04/03.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .systemBackground
+        window?.rootViewController = LoginViewController()
+
+        return true
+    }
+}
+
---
+++ HaLyShop/Login/LoginViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.swift
+//  HaLyShop
+//
+//  Created by Pham on 2024/04/03.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
